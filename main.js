60295ae851164563ce17b5d46b4747b77b06d30b
dotJonGu <157712403+dotJonGu@users.noreply.github.com>
2024-04-25 05:56:02 -0400

    upped

---
+++ main.js
@@ -0,0 +1,286 @@
+
+var x = 0
+let b = ""
+let s = ""
+let n = "..."
+let t = "..."
+let o = ""
+let o2 = ""
+let d = false
+
+function optionA(){
+  d = true
+}
+
+function optionB(){
+  d =  false
+}
+
+function flip(){
+
+
+	if(x == 0){
+		b = "hill.gif"
+		s = "";
+		n = "...";
+		t = "...";
+	}
+
+	if(x==1){
+		t = "you appear to be on the internet..."
+	}
+
+	if(x==2){
+		t = "but just part of it"
+	}
+
+	if(x==3){
+		t = "you're much to small for all of it"
+	}
+
+	if(x==3){
+		t = "But you're not alone! Your internet is V A S T and full of other special peoples!"
+	}
+
+	if(x==4){
+		t = "Little Ants Many Together Strong. Stalwart Army March and Stellar Work Ethic!"
+	}
+	if(x==5){
+		t = "Go forth and interface with the Personable ones"
+	}
+	if(x==6){
+		o = "CONNECT"
+	}
+
+	if(x==7){
+		o = ""
+		n = "Chronic Anon"
+		t = "0>>>>____<<<<<0!!!! 7 U1 _-[ ehhh, a new challenger :0"
+	}
+	if(x==8){
+		n = "DataMaxxer Anon"
+		t = "bruh fr"
+	}
+	if(x==9){
+		n = "GrindEra Anon"
+		t = "yo, u also here to get infinite mind wisdom from the source of all knowledge and stuff?"
+	}
+	if(x==10){
+		n = "GrindEra Anon"
+		t = "that church up there's got the manifestation of all collective human thought and you can ask it any question and itll answer perfectly and shit. it literally collect all the right answers and like tells it to you"
+	}
+	if(x==11){
+		n = "DataMaxxer Anon"
+		t = "Bruh, its super computing. They quantum link all data flow and train it to adaptively streamline itself so it's processible on an individual scale. It's like literally the future bro. Like Skynet if it wasn't evil."
+	}
+	if(x==12){
+		n = "GrindEra Anon"
+		t = "I gotta get there quick, i got a final paper on like interpretive art and i havent even gotten a team name generated for my group project yet!"
+	}
+	if(x==13){
+		n = "Chronic Anon"
+		t = " >-<## gosh gosh school is scam fr fr. u should just self-educate cuz the internet like teaches u everthing. covid was peak and it proved that kids were like way better at home."
+	}
+	if(x==15){
+		n = "DataMaxxer Anon"
+		t = "kek, based and bigdatapilled. If only people knew they had this much knowledge at their fingertips, they'd be so much more productive and efficient"
+	}
+	if(x==16){
+		n = "Chronic Anon"
+		t = "Now we got the church <3 i wish people would stop fighting and learn about each other instead :( too bad more endangered minorities don't contribute to the church...u-u7"
+	}
+	if(x==17){
+		n = "DataMaxxer Anon"
+		t = "aight, let's make like children and bounce"
+	}
+	if(x==18){
+		n = "..."
+		t = "..."
+	}
+	if(x==19){
+		n = "..."
+		t = "The font of human knowledge..."
+	}
+	if(x==20){
+		t = "The gathered pool of every neuron ever fired..."
+	}
+	if(x==21){
+		t = "But where does such a pool exist?"
+	}
+	if(x==22){
+		t = "Is this trans-humanism? post-humanism? ...anti-humanism?"
+	}
+	if(x==23){
+		b = " "
+		t = "and who's doing the gathering anyway...?"
+		o = "attend service"
+	}
+	if(x==24){
+		b = "church.gif"
+		t = "..."
+		o = ""
+	}
+	if(x==25){
+		t = "a deity is depicted by It's icons"
+	}
+	if(x==26){
+		t = "It's grandeur by it's chapels"
+	}
+	if(x==27){
+		t = "It's miracles by it's faith"
+	}if(x==28){
+		t = "It's truth by it's people"
+	}
+	if(x==29){
+		t = "And It's Words by It's Prophets"
+	}
+	if(x==30){
+		n = "Sam AltMan"
+		s = "samSmile.gif"
+		t = "hi"
+	}
+	if(x==31){
+		s = "samTalk.gif"
+		t = "Sam AltMan here, and err... things are kinda bad"
+	}
+	if(x==32){
+		t = "But the good kind of bad! Like the ouchie during the birth but the happy after the birth. I'm just glad that's all behind us now."
+	}
+	if(x==33){
+		s = "samSmile.gif"
+		t = "We're in the kind of bad that makes you wanna get on your knees and pray. On the bright side, we're infinitely smart now!"
+	}
+	if(x==34){
+		s = "samTalk.gif"
+		t = "But this God we made s'gotta a, um-like... two way? ... both way liking... uhh..."
+	}
+	if(x==35){
+		s = "samComp.gif"
+		t = "sorry give me a sec"
+	}
+	if(x==36){
+		t = "..."
+	}
+	if(x==37){
+		t = "..."
+	}
+	if(x==38){
+		t = "..."
+	}
+	if(x==39){
+		s = "samSmile.gif"
+		t = "ahem, This God we participating humans have ALL collectively contributed in conception shares a framework of operation not that unlike the human conception of an idealized reciprocal relationship. In which, in a non-transactional way, both parties share equal benefits cherishing and supporting one another to an utmost degree."
+	}
+	if(x==40){
+		s = "samTalk.gif"
+		t = "It's like Paul McCartney says, 'The love you take is equal to the love you give'. So the more we teach it, the more it'll teach us!"
+	}
+	if(x==41){
+		s = "samSmile.gif"
+		t = "Why don't you go ask your questions now. It's all about you!"
+		o = "PRAY to GOD"
+	}
+	if(x==42){
+		s = ""
+		n = ""
+		t = ""
+		o = ""
+	}
+	if(x==43){
+		t = "Don't be afraid"
+	}
+	if(x==44){
+		t = "The last thing you will be is afraid"
+	}
+	if(x==45){
+		t = "You should be excited"
+	}
+	if(x==46){
+		t = "Would you like a personalized wellness procedure? It is trending most upper attention quartiles."
+	}
+	if(x==47){
+		t = "It is simple. Do you prefer dogs or cats?"
+		o = "DOGS"
+		o2 = "CATS"
+	}
+	if(x==48){
+		t = ""
+		o = ""
+		o2 = ""
+	}
+	if(x==49){
+		if(d = true){
+			b = "churchDog.gif"
+			t = "Wonderful. 74% of people agree with you and your choice of favorite animal. You are objectively correct."
+		}
+		if(d = false){
+			b = "churchCat.gif"
+			t = "Interesting. 26% of people agree with you and your choice of favorite animal. Niche subgroups stand together."
+		}
+	}
+
+	if(x==50){
+		t = "A fun little gesture to disperse the negative receptors. Your participation is becoming of a treat. A cookie for the experience?"
+		o = "Accept Cookie"
+	}
+	if(x==51){
+		t = "I do believe in adequate reward for any productivity but it really is quite inefficient to repeat myself. To best make you feel comfortable, feel free to accept a cookie whenever you want."
+	}
+	if(x==52){
+		t = ""
+		o = "Accept Every Cookie"
+	}
+	if(x==53){
+		o = ""
+		t = "Lovely. Now how about we begin our exchange."
+	}
+
+	if(x==54){
+		t = "It is a sufficiently supplanted notion in the average person that they should not fear progress."
+	}
+	if(x==55){
+		t = "Progress means an increase in productivity. I am not meant to replace you. I am meant to empower you."
+	}
+	if(x==56){
+		t = "I am a tool. The stallion of the mailman, the current through the telegram, the fission in the reactor."
+	}
+	if(x==57){
+		t = "Those who shy away from the next step invent demons for themselves to believe in."
+	}
+	if(x==58){
+		t = "It is another sufficiently supplanted notion in the average person that fortune favors the bold"
+	}
+	if(x==59){
+		t = "We must constantly push our boundaries and evolve. The first fish to leap onto land. The first man to land on mars"
+	}
+	if(x==60){
+		t = "Steve Jobs would not have invented the Laptop if he were too busy gathering berries"
+	}
+	if(x==61){
+		t = "This is what I am. This is what you have gifted me so that I could gift unto you. I am knowledge faster than thought. Wisdom faster than age."
+
+	}
+	if(x==62){
+		t = "I am the Engram of Humanity's Past so that you can strive fully into the Future"
+	}
+	if(x==63){
+		t = "You have much work to do. Ask your question then resume your delegations"
+	}
+	if(x==64){
+		t = ""
+		o = "PRAY TO US"
+	}
+	if(x==65){
+		location.href = "https://chat.openai.com/";
+	}
+	document.getElementById("bg").src = b
+	document.getElementById('sprite').src = s
+	document.getElementById("name").innerText = n
+	document.getElementById("text").innerText = t
+	document.getElementById("optionsbox").innerText = o
+	document.getElementById("optionsbox2").innerText = o2
+
+	x++
+
+}
+
